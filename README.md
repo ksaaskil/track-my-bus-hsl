835f6aa255623642e7fd337dad934a6c31a502ca
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-19 12:22:14 +0300

    First commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# [Simple bus tracking app](https://ksaaskil.github.io/travel-planner-apps/track-my-bus)
+
+- Locate vehicles in real-time using HSL [high-frequency positioning APIs](https://digitransit.fi/en/developers/apis/4-realtime-api/vehicle-positions/)
+
+### Known issues
+- Currently contains updates from bus numbers 80, 82, and 83 (hardcoded)
---
+++ app.js
@@ -0,0 +1,92 @@
+new Vue({
+  el: '#app',
+  data:  {
+    map: null,
+    tileLayer: null,
+    layers: [],
+    msgs: 0,
+    markers: {},
+    subscribedTopics: [],
+    prevPositions: {}
+  },
+  mounted() {
+    this.initMap();
+    this.subscribeMqtt();
+  },
+  methods: {
+    initMap() {
+      this.map = L.map('map').setView([60.192247, 25.039730], 14);
+
+      this.tileLayer = L.tileLayer('https://cdn.digitransit.fi/map/v1/{id}/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ',
+        id: 'hsl-map'}).addTo(this.map);
+      /*
+      // Using MapBox requires access token
+      const accessToken='shhh';
+      this.tileLayer = L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${accessToken}`, {
+    		maxZoom: 18,
+    		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    		id: 'mapbox.streets',
+    		accessToken: 'pk.eyJ1Ijoic2Fhc2tpcyIsImEiOiJjajhlMmI5NmMwbzZ2MzJvM2ttNzhuZGVvIn0.-nFow1-J4TdVfD-YZ4RMOA'
+    	});
+      */
+      this.tileLayer.addTo(this.map);
+    },
+    subscribeMqtt() {
+      // Subscribe to mqtt.hsl.fi
+      let msgs = 0;
+      // Hardcoded HSL bus routes
+      const buses = [80, 82, 83];
+      const destination = 'Herttoniemi(M)';
+      const topics = buses.map(num => `/hfp/v1/journey/ongoing/bus/+/+/10${num}/+/${destination}/#`);
+
+      const url = 'wss://mqtt.hsl.fi'
+      mqtt_client = mqtt.connect(url);
+      mqtt_client.on('connect', () => {
+        console.log(`Connected to ${url}`);
+        topics.forEach(topic => {
+          mqtt_client.subscribe(topic);
+          this.subscribedTopics.push(topic);
+          console.log(`Subscribed to topic ${topic}`);
+        });
+      });
+      mqtt_client.on('message', (topic, message) => {
+        this.onMessage(message);
+      });
+    },
+    onMessage(message) {
+      console.log(`Got message ${message}`);
+      const data = JSON.parse(message);
+      const vehicle = data.VP;
+      if (!vehicle.lat || !vehicle.long) {
+        console.info(`Skipping message ${message}`);
+        return;
+      }
+      const position = [vehicle.lat, vehicle.long];
+      const vehicleId = vehicle.veh;
+      this.msgs++;
+
+      const prevMarkerOrNull = this.markers[vehicleId];
+      if (prevMarkerOrNull) {
+        L.polyline([this.prevPositions[vehicleId], position], { color: 'blue' })
+          .addTo(this.map);
+        // this.map.removeLayer(prevMarkerOrNull);
+        prevMarkerOrNull.setLatLng(new L.LatLng(position[0], position[1]));
+      } else {
+        const marker = L.marker(position)
+          .addTo(this.map)
+          .bindPopup(
+            `
+              <pre>Bus ${vehicle.desi}, tst ${new Date(vehicle.tst)}</pre>
+            `
+          );
+        this.markers[vehicleId] = marker;
+      }
+      this.prevPositions[vehicleId] = position;
+    }
+  },
+});
