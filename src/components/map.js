056d744d534e88c6b1e0b210b657d0ce2b6963fc
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-19 14:57:52 +0300

    Add subscription.

--- src/components/map.js
+++ src/components/map.js
@@ -1,10 +1,31 @@
-import React, { useState } from "react";
-const { Map: LeafletMap, TileLayer, Marker, Popup } = require("react-leaflet");
+import React, { useEffect } from "react";
+import { subscribeMqtt } from "../lib/mqtt";
+const { Map: LeafletMap, TileLayer } = require("react-leaflet");
 
-export default ({ lat = 60.170996, lng = 24.938674 }) => {
+const onMessage = msg => {
+  const parsed = JSON.parse(msg);
+  if (!parsed.VP) {
+    return;
+  }
+  console.log(`Got position: ${JSON.stringify(parsed.VP)}`);
+};
+
+const defaultLatLng = [60.196195, 25.059663];
+const defaultZoom = 17;
+
+export default ({
+  lat = defaultLatLng[0],
+  lng = defaultLatLng[1],
+  zoom = defaultZoom,
+}) => {
+  useEffect(() => {
+    const unsubscribe = subscribeMqtt(onMessage);
+    return () => unsubscribe();
+  }, []);
+
   return typeof window !== "undefined" ? (
     <div className="map-container">
-      <LeafletMap center={[lat, lng]} zoom={17}>
+      <LeafletMap center={[lat, lng]} zoom={zoom}>
         <TileLayer
           url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
---
+++ src/lib/mqtt.js
@@ -0,0 +1,67 @@
+const mqtt = require("mqtt");
+/* 
+export function onMessage(message) {
+  console.log(`Got message ${message}`);
+  const data = JSON.parse(message);
+  const vehicle = data.VP;
+  if (!vehicle.lat || !vehicle.long) {
+    console.info(`Skipping message ${message}`);
+    return;
+  }
+  const position = [vehicle.lat, vehicle.long];
+  const vehicleId = vehicle.veh;
+  this.msgs++;
+
+  const prevMarkerOrNull = this.markers[vehicleId];
+  if (prevMarkerOrNull) {
+    L.polyline([this.prevPositions[vehicleId], position], {
+      color: "blue",
+    }).addTo(this.map);
+    // this.map.removeLayer(prevMarkerOrNull);
+    prevMarkerOrNull.setLatLng(new L.LatLng(position[0], position[1]));
+  } else {
+    const marker = L.marker(position)
+      .addTo(this.map)
+      .bindPopup(
+        `
+              <pre>Bus ${vehicle.desi}, tst ${new Date(vehicle.tst)}</pre>
+            `
+      );
+    this.markers[vehicleId] = marker;
+  }
+  this.prevPositions[vehicleId] = position;
+} */
+
+export function subscribeMqtt(onMessage) {
+  // Subscribe to mqtt.hsl.fi
+  let msgs = 0;
+  // Hardcoded HSL bus routes
+  const buses = [80, 82, 83];
+  const destination = "Herttoniemi(M)";
+  const topics = buses.map(
+    num => `/hfp/v1/journey/ongoing/bus/+/+/10${num}/+/${destination}/#`
+  );
+
+  const subscribedTopics = [];
+
+  const url = "wss://mqtt.hsl.fi";
+  const mqtt_client = mqtt.connect(url);
+  console.log(`Subscribing ${url}`);
+  mqtt_client.on("connect", () => {
+    console.log(`Connected to ${url}`);
+    topics.forEach(topic => {
+      mqtt_client.subscribe(topic);
+      subscribedTopics.push(topic);
+      console.log(`Subscribed to topic ${topic}`);
+    });
+  });
+  mqtt_client.on("message", (topic, message) => {
+    onMessage(message);
+  });
+  return () => {
+    subscribedTopics.forEach(topic => {
+      mqtt_client.unsubscribe(topic);
+      console.log(`Unsubscribed from topic ${topic}`);
+    });
+  };
+}
